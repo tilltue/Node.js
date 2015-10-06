f0b2bd6c99403c7a7a759ddddfbf40a2e4884752
tilltue <tilltue@gamil.com>
2015-10-06 10:52:27 +0900

    update

--- routes/route_web.js
+++ routes/route_web.js
@@ -1,12 +1,12 @@
 var Iconv  = require('iconv').Iconv;
 var url = require('url');
 var fs = require('fs');
-
+var prePath = '/home/tilltue/Git/Node.js/html/'
 module.exports = function(app) {
 	app.get('/webNews',function(req,res){
 		var query = url.parse(req.url, true).query;
   		console.log('get query = ', query)
-  		var path = query.path;
+  		var path = prePath + query.path;
   		fs.readFile(path, function (err, html) {
     		if (err) {
         		res.send('File Read Error', 404);
